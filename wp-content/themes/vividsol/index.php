98f5f978c1e655a72fbce9ebc0351dcf63ff4444
osmanfayyaz <osmanfayyaz32@gmail.com>
2020-08-08 05:56:50 -0700

    post title

--- wp-content/themes/vividsol/index.php
+++ wp-content/themes/vividsol/index.php
@@ -1,3 +1,22 @@
-<?php get_header();?>
-    
-<?php get_footer(); ?>
+    <?php get_header();?>
+        
+        <?php if(have_posts()) { ?>
+            <?php while(have_posts()) { ?>
+            <?php the_post(); ?>
+            <h2>
+                <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
+                    <?php the_title() ?>
+                </a>
+            </h2>
+            <div>
+                Posted on 
+                <a href="<?php echo get_permalink() ?>">
+                <time><?php echo get_the_date()?></time>
+                </a>
+            </div>
+            <?php } ?>
+    <?php } else { ?>
+    <p>sorry nothing match the criteria</p>
+    <?php } ?>
+
+    <?php get_footer(); ?>
