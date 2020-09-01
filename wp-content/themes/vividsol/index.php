7f4a77d741d0375bf710d69760a2d3ad140cf794
osmanfayyaz <osmanfayyaz32@gmail.com>
2020-09-01 09:09:42 -0700

    read more and pagination

--- wp-content/themes/vividsol/index.php
+++ wp-content/themes/vividsol/index.php
@@ -11,12 +11,20 @@
             <div>
                 Posted on 
                 <a href="<?php echo get_permalink() ?>">
-                <time datetime="<?php echo get_the_date('c') ?>"><?php echo get_the_date();?></time>
+                <time datetime="<?php echo get_the_date('c') ?>">
+                <?php echo get_the_date();?></time>
                 </a>
                 By <a href="<?php echo get_author_posts_url(get_the_author_meta("ID"))?>">
                 <?php echo get_the_author(); ?></a>
             </div>
-            <?php } ?>
+            <div>
+            <?php the_excerpt(  )?>
+            </div>
+            <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
+                   Read More <span class="u-screen-reader-text">About <?php the_title()?></span>
+                </a>
+                <?php } ?>
+                <?php the_posts_pagination(); ?>
     <?php } else { ?>
     <p>sorry nothing match the criteria</p>
     <?php } ?>
